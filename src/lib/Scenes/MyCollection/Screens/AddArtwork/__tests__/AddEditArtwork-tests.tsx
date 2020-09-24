49701f94eb01505177e41cf987b8366c0a401b5d
Christopher Pappas <damassi.pappas@gmail.com>
2020-09-23 17:04:50 -0700

    WIP add tests

--- src/lib/Scenes/MyCollection/Screens/AddArtwork/__tests__/AddEditArtwork-tests.tsx
+++ src/lib/Scenes/MyCollection/Screens/AddArtwork/__tests__/AddEditArtwork-tests.tsx
@@ -1,5 +1,34 @@
+import { FancyModalHeader } from "lib/Components/FancyModal/FancyModalHeader"
+import { __appStoreTestUtils__ } from "lib/store/AppStore"
+import { renderWithWrappers } from "lib/tests/renderWithWrappers"
+import React from "react"
+import { Dimensions } from "react-native"
+import { AddEditArtwork, tests } from "../AddEditArtwork"
+import { ArtistAutosuggest } from "../Components/ArtistAutosuggest"
+import { MediumPicker } from "../Components/MediumPicker"
+
+jest.mock("../Form/useArtworkForm", () => ({
+  useArtworkForm: () => ({
+    values: {},
+    formik: {},
+  }),
+}))
+
 describe("AddEditArtwork", () => {
-  it("works", () => {
-    //
+  it("renders correct components", () => {
+    const wrapper = renderWithWrappers(<AddEditArtwork />)
+    const CompleteButton = wrapper.root.findByProps({ "data-test": "CompleteButton" })
+    const expected = [
+      FancyModalHeader,
+      ArtistAutosuggest,
+      MediumPicker,
+      Dimensions,
+      tests.PhotosButton,
+      tests.AdditionalDetailsButton,
+      CompleteButton,
+    ]
+    expected.forEach((Component) => {
+      expect(Component).toBeDefined()
+    })
   })
 })
